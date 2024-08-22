ddcd13a586bf62c62b5bd71a8a20f0bbd285cef5
Yevhen Kakaraza <yevhen.kakaraza@netpower.no>
2024-08-22 22:52:25 +0300

    feat: dockerfile update

--- src/core/utils/environment/index.ts
+++ src/core/utils/environment/index.ts
@@ -1,6 +1,6 @@
 const environment = {
-  apiUrl: process.env.NEXT_PUBLIC_API_URL,
-  signalrEndpoint: process.env.NEXT_PUBLIC_SIGNALR_URL,
+  apiUrl: "http://localhost:8080",
+  signalrEndpoint: "/tradehub",
 };
 
 export default environment;
